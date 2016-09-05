1b9fba24c832cf00b0445810b891036d73516e60
Stijn <stijn.vanderlinden@student.fontys.nl>
2016-09-05 11:08:14 +0200

    Actors plaatsen op juiste plek en naam geven. Lijnen worden in list gezet.

--- Use Case/Form1.Designer.cs
+++ Use Case/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btActor = new System.Windows.Forms.Button();
             this.rbLine = new System.Windows.Forms.RadioButton();
             this.rbUseCase = new System.Windows.Forms.RadioButton();
-            this.rbActor = new System.Windows.Forms.RadioButton();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.rbSelect = new System.Windows.Forms.RadioButton();
             this.rbCreate = new System.Windows.Forms.RadioButton();
@@ -39,23 +39,39 @@
             this.btClear = new System.Windows.Forms.Button();
             this.btRemove = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtInsert = new System.Windows.Forms.RichTextBox();
+            this.Actor1 = new System.Windows.Forms.PictureBox();
+            this.Actor2 = new System.Windows.Forms.PictureBox();
+            this.Actor3 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Actor1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Actor2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Actor3)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btActor);
             this.groupBox1.Controls.Add(this.rbLine);
             this.groupBox1.Controls.Add(this.rbUseCase);
-            this.groupBox1.Controls.Add(this.rbActor);
             this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(93, 91);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Elementen";
+            // 
+            // btActor
+            // 
+            this.btActor.Location = new System.Drawing.Point(7, 17);
+            this.btActor.Name = "btActor";
+            this.btActor.Size = new System.Drawing.Size(75, 23);
+            this.btActor.TabIndex = 2;
+            this.btActor.Text = "Actor";
+            this.btActor.UseVisualStyleBackColor = true;
+            this.btActor.Click += new System.EventHandler(this.btActor_Click);
             // 
             // rbLine
             // 
@@ -79,22 +95,11 @@
             this.rbUseCase.Text = "Use Case";
             this.rbUseCase.UseVisualStyleBackColor = true;
             // 
-            // rbActor
-            // 
-            this.rbActor.AutoSize = true;
-            this.rbActor.Location = new System.Drawing.Point(7, 20);
-            this.rbActor.Name = "rbActor";
-            this.rbActor.Size = new System.Drawing.Size(50, 17);
-            this.rbActor.TabIndex = 0;
-            this.rbActor.TabStop = true;
-            this.rbActor.Text = "Actor";
-            this.rbActor.UseVisualStyleBackColor = true;
-            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.rbSelect);
             this.groupBox2.Controls.Add(this.rbCreate);
-            this.groupBox2.Location = new System.Drawing.Point(112, 13);
+            this.groupBox2.Location = new System.Drawing.Point(221, 14);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(93, 91);
             this.groupBox2.TabIndex = 1;
@@ -128,14 +133,14 @@
             // 
             this.pictureBox.Location = new System.Drawing.Point(12, 111);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(982, 498);
+            this.pictureBox.Size = new System.Drawing.Size(804, 559);
             this.pictureBox.TabIndex = 2;
             this.pictureBox.TabStop = false;
             this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
             // 
             // btClear
             // 
-            this.btClear.Location = new System.Drawing.Point(919, 13);
+            this.btClear.Location = new System.Drawing.Point(741, 14);
             this.btClear.Name = "btClear";
             this.btClear.Size = new System.Drawing.Size(75, 23);
             this.btClear.TabIndex = 3;
@@ -145,7 +150,7 @@
             // 
             // btRemove
             // 
-            this.btRemove.Location = new System.Drawing.Point(919, 42);
+            this.btRemove.Location = new System.Drawing.Point(741, 43);
             this.btRemove.Name = "btRemove";
             this.btRemove.Size = new System.Drawing.Size(75, 23);
             this.btRemove.TabIndex = 4;
@@ -156,26 +161,62 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(211, 12);
+            this.label1.Location = new System.Drawing.Point(112, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 6;
             this.label1.Text = "Tekst:";
             // 
-            // richTextBox1
+            // txtInsert
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(214, 29);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 53);
-            this.richTextBox1.TabIndex = 7;
-            this.richTextBox1.Text = "";
+            this.txtInsert.Location = new System.Drawing.Point(115, 31);
+            this.txtInsert.Name = "txtInsert";
+            this.txtInsert.Size = new System.Drawing.Size(100, 53);
+            this.txtInsert.TabIndex = 7;
+            this.txtInsert.Text = "";
+            // 
+            // Actor1
+            // 
+            this.Actor1.BackColor = System.Drawing.Color.White;
+            this.Actor1.BackgroundImage = global::Use_Case.Properties.Resources.Actor;
+            this.Actor1.Location = new System.Drawing.Point(20, 120);
+            this.Actor1.Name = "Actor1";
+            this.Actor1.Size = new System.Drawing.Size(112, 125);
+            this.Actor1.TabIndex = 8;
+            this.Actor1.TabStop = false;
+            this.Actor1.Visible = false;
+            // 
+            // Actor2
+            // 
+            this.Actor2.BackColor = System.Drawing.Color.White;
+            this.Actor2.BackgroundImage = global::Use_Case.Properties.Resources.Actor;
+            this.Actor2.Location = new System.Drawing.Point(20, 305);
+            this.Actor2.Name = "Actor2";
+            this.Actor2.Size = new System.Drawing.Size(112, 125);
+            this.Actor2.TabIndex = 9;
+            this.Actor2.TabStop = false;
+            this.Actor2.Visible = false;
+            // 
+            // Actor3
+            // 
+            this.Actor3.BackColor = System.Drawing.Color.White;
+            this.Actor3.BackgroundImage = global::Use_Case.Properties.Resources.Actor;
+            this.Actor3.Location = new System.Drawing.Point(20, 488);
+            this.Actor3.Name = "Actor3";
+            this.Actor3.Size = new System.Drawing.Size(112, 125);
+            this.Actor3.TabIndex = 10;
+            this.Actor3.TabStop = false;
+            this.Actor3.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1006, 621);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(828, 682);
+            this.Controls.Add(this.Actor3);
+            this.Controls.Add(this.Actor2);
+            this.Controls.Add(this.Actor1);
+            this.Controls.Add(this.txtInsert);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btRemove);
             this.Controls.Add(this.btClear);
@@ -184,11 +225,15 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Use Case";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Actor1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Actor2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Actor3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -199,7 +244,6 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton rbLine;
         private System.Windows.Forms.RadioButton rbUseCase;
-        private System.Windows.Forms.RadioButton rbActor;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.RadioButton rbSelect;
         private System.Windows.Forms.RadioButton rbCreate;
@@ -207,7 +251,11 @@
         private System.Windows.Forms.Button btClear;
         private System.Windows.Forms.Button btRemove;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox txtInsert;
+        private System.Windows.Forms.Button btActor;
+        private System.Windows.Forms.PictureBox Actor1;
+        private System.Windows.Forms.PictureBox Actor2;
+        private System.Windows.Forms.PictureBox Actor3;
     }
 }
 
--- Use Case/Form1.cs
+++ Use Case/Form1.cs
@@ -16,8 +16,11 @@ namespace Use_Case
         Graphics blad;
         Pen p;
         int i = 0;
+        int j = 0;
         int x1, x2, y1, y2;
-        List<Image> Images = new List<Image>();
+        Font myFont = new Font("Arial", 14);
+        List<List<int>> lijnen = new List<List<int>>();
+        List<int> Images = new List<int>();
 
         public Form1()
         {
@@ -27,14 +30,48 @@ namespace Use_Case
             pictureBox.BackColor = Color.White;
         }
 
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(List<int> list in lijnen)
+            {
+                blad.DrawLine(p, list[0], list[1], list[2], list[3]);
+            }
+        }
+
+        private void btActor_Click(object sender, EventArgs e)
+        {
+            if(j > 2)
+            {
+                MessageBox.Show("Kan niet meer Actors toevoegen.");
+            }
+            if(j == 2)
+            {
+                Actor3.Visible = true;
+                j++;
+                blad.DrawString(txtInsert.Text, myFont, Brushes.Black, 23, 510);
+            }
+            if(j == 1)
+            {
+                Actor2.Visible = true;
+                j++;
+                blad.DrawString(txtInsert.Text, myFont, Brushes.Black, 23, 330);
+            }
+            if(j == 0)
+            {
+                Actor1.Visible = true;
+                j++;
+                blad.DrawString(txtInsert.Text, myFont, Brushes.Black, 23, 140);
+            }
+        }
+
         private void pictureBox_Click(object sender, EventArgs e)
         {
+            MouseEventArgs muis = (MouseEventArgs)e;
             if (rbCreate.Checked)
             {
-                MouseEventArgs muis = (MouseEventArgs)e;
                 if (rbLine.Checked)
                 {
-                    
+
                     if (i == 0)
                     {
                         i++;
@@ -47,28 +84,25 @@ namespace Use_Case
                         x2 = muis.X;
                         y2 = muis.Y;
                         blad.DrawLine(p, x1, y1, x2, y2);
-                        Image image = new Image("Line", x1, x2, y1, y2);
-                        Images.Add(image);
+                        lijnen.Add(new List<int> { x1, y1, x2, y2 });
                     }
                 }
-                else if (rbActor.Checked)
+                else
                 {
                     x1 = muis.X;
                     y1 = muis.Y;
-                    blad.DrawEllipse(p, x1 - 15, y1 - 30, 30, 30);
-                    blad.DrawLine(p, x1, y1, x1, y1 + 40);
-                    blad.DrawLine(p, x1, y1, x1 - 25, y1 + 25);
-                    blad.DrawLine(p, x1, y1, x1 + 25, y1 + 25);
-                    blad.DrawLine(p, x1, y1 + 40, x1 - 25, y1 + 65);
-                    blad.DrawLine(p, x1, y1 + 40, x1 + 25, y1 + 65);
-                    Image image = new Image("Actor", x1, y1);
                 }
+            }
+
+            else
+            {
             }
         }
 
         private void btClear_Click(object sender, EventArgs e)
         {
             blad.Clear(Color.White);
+            Images.Clear();
         }
 
         private void btRemove_Click(object sender, EventArgs e)
